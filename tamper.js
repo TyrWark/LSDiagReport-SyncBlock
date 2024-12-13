9035273d493f002fc2b3df6bbe940ba9015cfb4d
TyrWark <167569189+TyrWark@users.noreply.github.com>
2024-12-13 12:06:17 -0600

    Create tamper.js

---
+++ tamper.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Diagnostic Links for BackOffice + Size
+// @namespace    http://tampermonkey.net/
+// @version      2.0
+// @description  Adds links for easy access to Diag reports. Now with Subscription Email Checking and blocks sync of problematic accounts.
+// @author       Ty Wark
+// @match        https://lightspeedanalytics.net/cl_accounts/settings
+// @match        https://app.lightspeedanalytics.net/embed/explore/*
+// @match        https://app.lightspeedanalytics.net/embed/*
+// @match        https://lightspeedanalytics.net/back_office*
+// @require      https://raw.githubusercontent.com/TyrWark/LSDiagReport-SyncBlock/refs/heads/main/include.js
+// @run-at      document-idle
+// @grant       GM_addStyle
+// ==/UserScript==
+
+//Documentation found here https://lightspeedhq.atlassian.net/wiki/spaces/CLOUD/pages/139441487/Looker+Diagnostic+Analytics+Reports
+
